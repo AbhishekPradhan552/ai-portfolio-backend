fd905cd616ab0372d38a9958ff4d0f313aa7f6f9
pradhanlearning <abhipradhan552@gmail.com>
2026-02-23 05:12:08 +0530

    Production DB config

--- database.py
+++ database.py
@@ -15,6 +15,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
+    pool_pre_ping=True,
+    connect_args={"ssl": True},
 )
 
 AsyncSessionLocal = async_sessionmaker(
